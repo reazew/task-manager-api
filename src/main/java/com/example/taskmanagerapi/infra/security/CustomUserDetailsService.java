fbc5213714c69980181be02c921661980bb00e17
Jonas <jonasmessias30@gmail.com>
2025-09-10 17:26:55 -0300

    [FEAT]: Adding CustomUserDetailsService

---
+++ src/main/java/com/example/taskmanagerapi/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.example.taskmanagerapi.infra.security;
+import com.example.taskmanagerapi.repositories.UserRepository;
+import com.example.taskmanagerapi.domain.user.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
