5b85168d08304d2232103247af49425604a78a12
Jonas <jonasmessias30@gmail.com>
2025-09-12 00:01:50 -0300

    [FIX]: Fixing secret key

--- src/main/java/com/example/taskmanagerapi/infra/security/TokenService.java
+++ src/main/java/com/example/taskmanagerapi/infra/security/TokenService.java
@@ -1,16 +1,17 @@
 package com.example.taskmanagerapi.infra.security;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.example.taskmanagerapi.domain.user.User;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
@@ -18,7 +19,7 @@ public class TokenService {
     public String secret;
      public String generateToken(User user){
          try {
-             Algorithm algorithm = Algorithm.HMAC256(user.getPassword());
+             Algorithm algorithm = Algorithm.HMAC256(secret);
 
              String token = JWT.create()
                      .withIssuer("login-auth-api")
