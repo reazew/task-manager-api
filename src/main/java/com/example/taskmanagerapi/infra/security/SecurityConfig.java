d72feb7004558f89f48977d2c98fb4475df25866
Jonas <jonasmessias30@gmail.com>
2025-10-21 19:17:45 -0300

    [FEAT]: Allow public access to password reset routes

--- src/main/java/com/example/taskmanagerapi/infra/security/SecurityConfig.java
+++ src/main/java/com/example/taskmanagerapi/infra/security/SecurityConfig.java
@@ -33,6 +33,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/forgot-password").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/reset-password").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
